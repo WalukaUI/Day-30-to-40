e44385a1377ddf59a1cae758754b3a3893c0a7cc
walukaui <cwaluka@yahoo.com>
2024-05-15 17:26:44 -0500

    "Flash card project initial files"

---
+++ Day31 project/main.py
@@ -0,0 +1,20 @@
+from tkinter import *
+from tkinter import messagebox
+import random
+from math import *
+
+BACKGROUND_COLOR = "#B1DDC6"
+window = Tk()
+window.title("Flash cards")
+window.config(padx=50, pady=50)
+canvas = Canvas(width=200, height=200)
+
+
+
+
+
+
+
+
+window.mainloop()
+
