09e9da0e9c0802244f12050d16acfa967cc10c74
walukaui <cwaluka@yahoo.com>
2024-05-19 15:00:40 -0500

    "Flash card functionality"

--- Day31 project/main.py
+++ Day31 project/main.py
@@ -5,35 +5,44 @@ import random
 import pandas
 from math import *
 
-
-
+file = pandas.read_csv("data/JPwords - Sheet1.csv")
+to_learn = file.to_dict(orient="records")
+current_card_num = 0
 
 
 def next_card():
-    file = pandas.read_csv("data/JPwords - Sheet1.csv")
-    to_learn = file.to_dict(orient="records")
-    selection = random.randrange(1, len(to_learn))
-    canvas.itemconfig(card_title, text="Japanease")
-    canvas.itemconfig(card_word, text=to_learn[selection]["JPWORD"])
+    global current_card_num, flip_timer
+    window.after_cancel(flip_timer)
+    current_card_num = random.randrange(1, len(to_learn))
+    canvas.itemconfig(card_title, text="Japanease", fill="black")
+    canvas.itemconfig(card_word, text=to_learn[current_card_num]["JPWORD"], fill="black")
+    canvas.itemconfig(jp_img, image=front_img)
+    flip_timer = window.after(ms=3000, func=flip_card)
 
 
+def flip_card():
+    canvas.itemconfig(jp_img, image=card_back)
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=to_learn[current_card_num]["ENGWORD"], fill="white")
 
 
 #UI_________________________________
-
 
 
 BACKGROUND_COLOR = "#B1DDC6"
 window = Tk()
 window.title("Flash cards")
 window.config(padx=50, pady=50, background=BACKGROUND_COLOR)
+flip_timer = window.after(ms=3000, func=flip_card)
+
 canvas = Canvas(width=800, height=526)
-bk_img = PhotoImage(file="images/card_front.png")
-canvas.create_image(400, 263, image=bk_img)
+front_img = PhotoImage(file="images/card_front.png")
+jp_img = canvas.create_image(400, 263, image=front_img)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 canvas.grid(column=0, row=0, columnspan=3, rowspan=3)
 card_title = canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 250, text="Word", font=("Ariel", 60, "bold"))
+card_back = PhotoImage(file="images/card_back.png")
 
 
 unknown_btn = PhotoImage(file="images/wrong.png")
