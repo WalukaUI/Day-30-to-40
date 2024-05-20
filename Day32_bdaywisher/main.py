88ee6cdae15f9b70cf42f82437ebb4d3caced48a
walukaui <cwaluka@yahoo.com>
2024-05-20 17:26:32 -0500

    "automated birthday wishes initial files hard level"

--- Day32_bdaywisher/main.py
+++ Day32_bdaywisher/main.py
@@ -1,21 +1,22 @@
-import smtplib
-import datetime as dt
-import random
+##################### Hard Starting Project ######################
 
-PP = "xxxxxxxxxx"
-mailer = "xxxxxxxxxxxx"
+# 1. Update the birthdays.csv with your friends & family's details. 
+# HINT: Make sure one of the entries matches today's date for testing purposes. 
 
-now = dt.datetime.now()
-year = now.year
-month = now.month
-day_of_week = now.weekday()
-if day_of_week == 0:
-    with open("quotes.txt", encoding="utf8")as files:
-        quotes = files.readlines()
-        quote = random.choice(quotes)
-        with smtplib.SMTP("smtp.gmail.com") as connection:
-            connection.starttls()
-            connection.login(user=mailer, password=PP)
-            connection.sendmail(from_addr=mailer, to_addrs="cwaluka@yahoo.com", msg=f"Subject: Hello \n\n {quote}")
+# 2. Check if today matches a birthday in the birthdays.csv
+# HINT 1: Only the month and day matter. 
+# HINT 2: You could create a dictionary from birthdays.csv that looks like this:
+# birthdays_dict = {
+#     (month, day): data_row
+# }
+#HINT 3: Then you could compare and see if today's month/day matches one of the keys in birthday_dict like this:
+# if (today_month, today_day) in birthdays_dict:
+
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+# HINT: https://www.w3schools.com/python/ref_string_replace.asp
+
+# 4. Send the letter generated in step 3 to that person's email address.
+# HINT: Gmail(smtp.gmail.com), Yahoo(smtp.mail.yahoo.com), Hotmail(smtp.live.com), Outlook(smtp-mail.outlook.com)
+
 
 
